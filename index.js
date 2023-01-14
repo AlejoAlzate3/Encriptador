f3174acc944bdbec54333351588e103ace7d3632
AlejoAlzate3 <alejoalzate1@gmail.com>
2023-01-13 21:24:46 -0500

    Empezando a codificar el backend en JavaScript

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 let botonEncriptar = document.querySelector('.btn-encriptar');
 let botonDesencriptar = document.querySelector('.btn-desencriptar');
 
-function encriptar() {
-    let texto = document.querySelector('.texto').value;
-    let offset = document.querySelector('.offset').value;
-    let resultado = cipher.encode(offset, texto);
-    document.querySelector('.resultado').value = resultado;
-}
+//Empezar js encriptar botones
